58f0e511269fbadbf639f8573adb31d6b77f19ad
EKG-coder <kime2702@my.sduhsd.net>
2026-04-09 02:04:02 +0000

    Player Name

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+//Player Name
+let playerName = prompt("Enter your name: ", "");
 
-//Global Constants
+//EXTRA: First and Last Names
+if(playerName && playerName.trim() !== ""){
+    playerName = playerName.trim().toLowerCase();
+    let words = playerName.split("") // Separate each individual word into array
+    let result = "";
+
+    for (let i = 0; i < words.length; i++){
+        let word = words[i]
+        if (word.length > 0){
+            result += word.charAt(0).toUpperCase() + word.slice(1) + " "; // Capitalize first letter of word and add
+        }
+        playerName = result.trim(); // remove extra space at end 
+    }
+}
+
+//Global Variables
 let range = 0;
 let answer = 0;
 let guessCount = 0;
@@ -12,14 +29,14 @@ document.getElementById("guessBtn").addEventListener("click", makeGuess);
 function play(){
     let levels = document.getElementsByName("level");
     
-    for(let i=0; i<levels.length; i++){
+    for(let i = 0; i < levels.length; i++){
         if(levels[i].checked){
-            range = parseInt(levels[i].value)
+            range = parseInt(levels[i].value);
         }
-        levels[i].disabled = true
+        levels[i].disabled = true;
     }
-    document.getElementById("msg").textContent = "Guess a number 1-" + range;
-    answer = Math.floor(Math.random()*range)+1
+    document.getElementById("msg").textContent = playerName + ", guess a number 1-" + range;
+    answer = Math.floor(Math.random()*range)+1;
     guessCount = 0;
 
     guessBtn.disabled = false;
@@ -37,7 +54,7 @@ function makeGuess(){
     guessCount++;
 
     if(guess == answer){
-        msg.textContent = "Correct! It took " + guessCount + " tries.";
+        msg.textContent = "Correct " + playerName + "!" + " It took " + guessCount + " tries.";
         updateScore(guessCount);
         resetGame();
     }
